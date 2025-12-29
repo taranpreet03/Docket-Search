7d3cc534843f890691995dcf11cbd31642d6908f
taranpreet03 <taranpreet.kaur@omlogistics.co.in>
2025-12-29 16:39:16 +0530

    Data got changed according to users from Todo

--- script.js
+++ script.js
@@ -1,75 +1,66 @@
-const API_URL = "https://jsonplaceholder.typicode.com/todos";
+const API_URL = "https://jsonplaceholder.typicode.com/users/";
 
-// const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
 const resultsDiv = document.getElementById("results");
 
-let allTodos = [];
+let allUsers = [];
 
 // Load data 
-async function loadTodos() {
-  const response = await fetch(API_URL);
-  allTodos = await response.json();
-  displayTodos(allTodos); 
+async function loadUsers() {
+  try {
+    const response = await fetch(API_URL);
+    allUsers = await response.json();
+    displayUsers(allUsers);
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
 }
-
-function displayTodos(todos) {
+function displayUsers(users) {
   resultsDiv.innerHTML = "";
 
-  if (todos.length === 0) {
+  if (users.length === 0) {
     resultsDiv.innerHTML = `
       <tr>
-        <td colspan="4" class="empty">No results found</td>
+        <td colspan="5" class="empty">No results found</td>
       </tr>
     `;
     return;
   }
 
-  //Fetch
-  todos.forEach(todo => {
+  users.forEach(user => {
     const row = document.createElement("tr");
 
     row.innerHTML = `
-      <td>${todo.id}</td>
-      <td>${todo.userId}</td>
-      <td>${todo.title}</td>
-      <td class="${todo.completed ? "completed" : "pending"}">
-        ${todo.completed ? "Completed" : "Pending"}
-      </td>
+      <td>${user.id}</td>
+      <td>${user.name}</td>
+      <td>${user.email}</td>
+      <td>${user.phone}</td>
     `;
-
     resultsDiv.appendChild(row);
   });
 }
-
 // Search filter
 searchInput.addEventListener("input", () => {
   const query = searchInput.value.trim().toLowerCase();
 
-  // If input is empty, show all data
   if (query === "") {
-    displayTodos(allTodos);
+    displayUsers(allUsers);
     return;
   }
 
-  let filtered = [];
+  let filteredUsers;
 
-  // Search by ID
+  // ID
   if (!isNaN(query)) {
-    filtered = allTodos.filter(todo =>
-      todo.id === Number(query)
-    );
+    filteredUsers = allUsers.filter(user => user.id === Number(query));
   } 
-  // Search by title
+  //Name
   else {
-    filtered = allTodos.filter(todo =>
-      todo.title.toLowerCase().includes(query)
+    filteredUsers = allUsers.filter(user =>
+      user.name.toLowerCase().includes(query)
     );
   }
-
-  displayTodos(filtered);
+  displayUsers(filteredUsers);
 });
 
-
-
-loadTodos();
+loadUsers();
