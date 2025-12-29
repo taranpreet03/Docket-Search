f14bd235631177bde5db9e53e2ae64b07557fd96
taranpreet03 <taranpreet.kaur@omlogistics.co.in>
2025-12-29 11:16:09 +0530

    Display function is created.

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ async function fetchTodos() {
   const data = await response.json();
   return data;
 }
+
+//Display
+function displayTodos(todos) {
+  resultsDiv.innerHTML = "";
+
+  if (todos.length === 0) {
+    resultsDiv.innerHTML = "<p>No results found</p>";
+    return;
+  }
+
+  todos.forEach(todo => {
+    const div = document.createElement("div");
+    div.className = "todo-card";
+
+    div.innerHTML = `
+      <p><strong>ID:</strong> ${todo.id}</p>
+      <p><strong>Title:</strong> ${todo.title}</p>
+      <p class="${todo.completed ? 'completed' : 'pending'}">
+        Status: ${todo.completed ? "Completed" : "Pending"}
+      </p>
+    `;
+
+    resultsDiv.appendChild(div);
+  });
+}
